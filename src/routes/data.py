a25c91915d49928bf81b8f686e52eb05a9f3dd62
Ahmed Gamal Okka <ahmed.okka@hotmail.com>
2026-03-12 03:45:36 +0200

    Refactor upload_data function for improved readability and logging; utilize DataController instance for validation and filename generation

--- src/routes/data.py
+++ src/routes/data.py
@@ -1,11 +1,13 @@
+from models import ResponseStatus
 from fastapi import FastAPI, APIRouter, Depends, File, UploadFile, status
 from fastapi.responses import JSONResponse
 from helpers.config import get_settings, Settings
 import os
 from controllers import DataController, ProjectController
 import aiofiles
+import logging
+logger = logging.getLogger('uvicorn.error')
 
-from models import ResponseStatus
 
 data_router = APIRouter(
     prefix="/api/v1/data",
@@ -17,8 +19,9 @@ data_router = APIRouter(
 async def upload_data(project_id: str, file: UploadFile,
                       app_settings: Settings = Depends(get_settings)):
 
+    data_controller = DataController()
     # Validate file properties
-    is_valid, result_signal = DataController().validate_uploaded_file(file=file)
+    is_valid, result_signal = data_controller.validate_uploaded_file(file=file)
 
     if not is_valid:
         return JSONResponse(
@@ -28,14 +31,21 @@ async def upload_data(project_id: str, file: UploadFile,
 
     # Save the file to the specified location
     project_dir_oath = ProjectController().get_project_path(project_id=project_id)
-    file_path = os.path.join(
-        project_dir_oath,
-        file.filename
+    file_path = data_controller.generate_unique_filename(
+        original_filename=file.filename,
+        project_id=project_id
     )
-    async with aiofiles.open(file_path, 'wb') as out_file:
-        while chunk := await file.read(app_settings.FILE_DEFAULT_CHUNK_SIZE):
-            await out_file.write(chunk)
-
+    try:
+        async with aiofiles.open(file_path, 'wb') as out_file:
+            while chunk := await file.read(app_settings.FILE_DEFAULT_CHUNK_SIZE):
+                await out_file.write(chunk)
+    except Exception as e:
+        logger.error(f"Error while uploading file: {e}")
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content={
+                "message": ResponseStatus.FILE_UPLOAD_FAILURE.value}
+        )
     return JSONResponse(
 
         content={"message": ResponseStatus.FILE_UPLOAD_SUCCESS.value}
