f6d38764cab59b3a940d04fa51b3803883ac2450
Ahmed Gamal Okka <ahmed.okka@hotmail.com>
2026-03-31 14:20:22 +0200

    Fix type ignore comments and correct variable name in upload_data function for clarity

--- src/routes/data.py
+++ src/routes/data.py
@@ -1,10 +1,10 @@
 from models import ResponseStatus
-from fastapi import FastAPI, APIRouter, Depends, File, UploadFile, status
+from fastapi import FastAPI, APIRouter, Depends, UploadFile, status
 from fastapi.responses import JSONResponse
 from helpers.config import get_settings, Settings
 import os
 from controllers import DataController, ProjectController
-import aiofiles
+import aiofiles  # type: ignore
 import logging
 logger = logging.getLogger('uvicorn.error')
 
@@ -30,12 +30,13 @@ async def upload_data(project_id: str, file: UploadFile,
         )
 
     # Save the file to the specified location
-    project_dir_oath = ProjectController().get_project_path(project_id=project_id)
+    project_dir_path = ProjectController().get_project_path(project_id=project_id)
     if file.filename is not None:
         file_path = data_controller.generate_unique_filename(
             original_filename=file.filename,
             project_id=project_id
         )
+
     try:
         async with aiofiles.open(file_path, 'wb') as out_file:
             while chunk := await file.read(app_settings.FILE_DEFAULT_CHUNK_SIZE):
@@ -47,7 +48,7 @@ async def upload_data(project_id: str, file: UploadFile,
             content={
                 "message": ResponseStatus.FILE_UPLOAD_FAILURE.value}
         )
-    return JSONResponse(
 
+    return JSONResponse(
         content={"message": ResponseStatus.FILE_UPLOAD_SUCCESS.value}
     )
